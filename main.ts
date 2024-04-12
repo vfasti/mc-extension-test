148fae8f7541e56487a288d9b23cf4765484c223
vfasti <verena.fastenbauer@hak-bregenz.at>
2024-04-12 11:28:29 +0200

    simplifying the version: without input parameters

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
+/**
+ * M Extension for micro:bit
+ */
+
 //% color="#0fbc11" icon="\uf185"
 namespace fastiExtensions {
 
     //% group="Zufallszahlen"
-    //% block="Zufällige Zahl zwischen |%min (0) und |%max (100)"
+    //% block="Zufällige Zahl zwischen 0 und 100"
     //% subcategory="Zufallszahl" weight=100
-    export function getRandomValue(min?: number, max?: number): number {
-        if (min == undefined) {
-            min = 0;
-        }
-        if (max == undefined) {
-            max = 100;
-        }
-        return Math.random() * (max - min) + min;
+    export function getRandomValue(): number {
+        return Math.random() * (100 - 0) + 100;
     }
+
 }
+
+
+//% group="Zufallszahlen"
+//% block="Zufällige Zahl zwischen |%min (0) und |%max (100)"
+//% subcategory="Zufallszahl" weight=100
+// export function getRandomValue(min?: number, max?: number): number {
+//     if (min == undefined) {
+//         min = 0;
+//     }
+//     if (max == undefined) {
+//         max = 100;
+//     }
+//     return Math.random() * (max - min) + min;
+// }
